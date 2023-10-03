f07f5674c5dbce89c16cd5e76c2c0576902babf7
Katarzyna Szewczyk-Skrzynecka <k.szewczyk.skrzynecka@gmail.com>
2023-10-03 19:43:44 +0200

    correct code

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -12,17 +12,23 @@ function Form() {
       .get(
         `https://api.nbp.pl/api/exchangerates/rates/a/${selectValue}/?format=json`
       )
-      .then((response) => convertCurrency(response.data.rates[0].mid, event))
+      .then((response) => {
+        if (response.data.rates?.[0]?.mid) {
+          const inputValue = event.target.amountToBeCalculated.value;
+          convertCurrency(response.data.rates[0].mid, inputValue);
+        } else {
+          setOutput(`Wystąpił błąd`);
+        }
+      })
       .catch((error) => console.log(error));
   }
 
-  function convertCurrency(rate, event) {
-    const inputValue = event.target.amountToBeCalculated.value;
+  function convertCurrency(rate, inputValue) {
     const total = inputValue * rate;
     setOutput(`to ${total.toFixed(2)} PLN`);
   }
   return (
-    <form id="currency-container" onSubmit={getExchangeRate}>
+    <form onSubmit={getExchangeRate}>
       <div className={styles.amountInput}>
         <label htmlFor="amount-to-be-calculated">
           Podaj kwotę do przeliczeń:
@@ -33,23 +39,20 @@ function Form() {
           min="0.01"
           step="0.01"
           name="amountToBeCalculated"
-          id="amount-to-be-calculated"
         />
       </div>
       <div className={styles.currencySelect}>
         <label htmlFor="currency">Wybierz walutę:</label>
-        <select name="currency" id="currency">
+        <select name="currency">
           <option value="eur">EUR</option>
           <option value="usd">USD</option>
           <option value="chf">CHF</option>
         </select>
       </div>
-      <button className={styles.submitBtn} type="submit" id="btn">
+      <button className={styles.submitBtn} type="submit">
         Przelicz
       </button>
-      <output className={styles.resultOutput} id="result">
-        {output}
-      </output>
+      <output className={styles.resultOutput}>{output}</output>
     </form>
   );
 }
